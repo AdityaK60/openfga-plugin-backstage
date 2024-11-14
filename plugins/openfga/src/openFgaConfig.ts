85fd598d27469797e617e62d254ea977719b4128
AmbrishRamachandiran <ambrish.r@infosys.com>
2024-11-14 10:17:40 +0530

    Added openfga config
    Signed-off-by: AmbrishRamachandiran <ambrish.r@infosys.com>

--- plugins/openfga/src/openFgaConfig.ts
+++ plugins/openfga/src/openFgaConfig.ts
@@ -1,5 +1,5 @@
 export const openFgaConfig = {
     baseUrl: 'http://localhost:8080', //Replace with your actual OpenFGA base URL
-    storeId: "01J20QE9WMGWRRD7FSKJ703JJD", //Replace with your OpenFGA store ID
-    authorizationModelId: "01JAW9TRAASCJT070G92GHZV62", //Replace with your OpenFGA authorizationModelId ID
+    storeId: "01JCJ48298XEBEWW3ETRNN38G5", //Replace with your OpenFGA store ID
+    authorizationModelId: "01JCJ496X6H5XHX9CV9V9MYCJ4", //Replace with your OpenFGA authorizationModelId ID
   };
